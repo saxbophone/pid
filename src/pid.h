b2d4f344ecfee7333cb18d8436c61c1d1020459d
Joshua Saxby <saxbophone@users.noreply.github.com>
2016-01-03 19:41:16 +0000

    `extern "C"` to the rescue!

--- src/pid.h
+++ src/pid.h
@@ -12,50 +12,59 @@
 #ifndef SAXBOPHONE_PID_H
 #define SAXBOPHONE_PID_H
 
+#ifdef __cplusplus
+extern "C"{
+#endif
 
-typedef struct pid_calibration {
+
+    typedef struct pid_calibration {
+        /*
+         * struct PID_Calibration
+         * 
+         * Struct storing calibrated PID constants for a PID Controller
+         * These are used for tuning the algorithm and the values they take are
+         * dependent upon the application - (in other words, YMMV...)
+         */
+        double kp; // Proportional gain
+        double ki; // Integral gain
+        double kd; // Derivative gain
+    } PID_Calibration;
+
+
+    typedef struct pid_state {
+        /*
+         * struct PID_State
+         * 
+         * Struct storing the current state of a PID Controller.
+         * This is used as the input value to the PID algorithm function, which also
+         * returns a PID_State struct reflecting the adjustments suggested by the algorithm.
+         * 
+         * NOTE: The output field in this struct is set by the PID algorithm function, and
+         * is ignored in the actual calculations.
+         */
+        double actual; // The actual reading as measured
+        double target; // The desired reading
+        double time_delta; // Time since last sample/calculation - should be set when updating state
+        // The previously calculated error between actual and target (zero initially)
+        double previous_error;
+        double integral; // Sum of integral error over time
+        double output; // the modified output value calculated by the algorithm, to compensate for error
+    } PID_State;
+
+
     /*
-     * struct PID_Calibration
+     * PID Controller Algorithm implementation
      * 
-     * Struct storing calibrated PID constants for a PID Controller
-     * These are used for tuning the algorithm and the values they take are
-     * dependent upon the application - (in other words, YMMV...)
+     * Given a PID calibration for the P, I and D values and a PID_State for the current
+     * state of the PID controller, calculate the new state for the PID Controller and set
+     * the output state to compensate for any error as defined by the algorithm
      */
-    double kp; // Proportional gain
-    double ki; // Integral gain
-    double kd; // Derivative gain
-} PID_Calibration;
+    PID_State pid_iterate(PID_Calibration calibration, PID_State state);
 
 
-typedef struct pid_state {
-    /*
-     * struct PID_State
-     * 
-     * Struct storing the current state of a PID Controller.
-     * This is used as the input value to the PID algorithm function, which also
-     * returns a PID_State struct reflecting the adjustments suggested by the algorithm.
-     * 
-     * NOTE: The output field in this struct is set by the PID algorithm function, and
-     * is ignored in the actual calculations.
-     */
-    double actual; // The actual reading as measured
-    double target; // The desired reading
-    double time_delta; // Time since last sample/calculation - should be set when updating state
-    // The previously calculated error between actual and target (zero initially)
-    double previous_error;
-    double integral; // Sum of integral error over time
-    double output; // the modified output value calculated by the algorithm, to compensate for error
-} PID_State;
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
-
-/*
- * PID Controller Algorithm implementation
- * 
- * Given a PID calibration for the P, I and D values and a PID_State for the current
- * state of the PID controller, calculate the new state for the PID Controller and set
- * the output state to compensate for any error as defined by the algorithm
- */
-PID_State pid_iterate(PID_Calibration calibration, PID_State state);
-
-
+// end of header
 #endif
